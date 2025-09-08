734c67f7f11c2bb5ed53371981189e88c4693a96
depstor <depstor90@gmail.com>
2025-09-08 14:01:33 +0300

    spin

--- src/script/script.js
+++ src/script/script.js
@@ -1,1 +1,23 @@
 import "/src/sass/style.scss";
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+try {
+    new Swiper(".mySwiper", {
+        modules: [Navigation, Pagination],
+        direction: "vertical",
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        loop: true,
+        spaceBetween: 10,
+    });
+} catch (error) {
+
+}
